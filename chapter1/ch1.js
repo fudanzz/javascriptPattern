a58afe1e6b994ff3c59c645113c4454732c33d54
fudanzz <fudanzz@gmail.com>
2016-10-14 14:51:05 +0800

    update

--- chapter1/ch1.js
+++ chapter1/ch1.js
@@ -1,15 +1,18 @@
 function onReady() {
 
-    var clock = new Clock('clock', 480);
+    var clock = new com.ibm.Clock('clock', 480);
 
-    var clock2 = new Clock('clock2', 0, 'GMT');
+    var clock2 = new com.ibm.Clock('clock2', 0, 'GMT');
 }
 
 Date.prototype.updateSeconds = function () {
     this.setSeconds(this.getSeconds() + 1);
 }
 
-function Clock(id, offset, lable) {
+var com = com || {}
+com.ibm = com.ibm || {};
+
+com.ibm.Clock = function (id, offset, lable) {
 
     offset = offset || 0;
 
@@ -34,7 +37,7 @@ function Clock(id, offset, lable) {
 
 
 
-Clock.prototype.updateClock = function () {
+com.ibm.Clock.prototype.updateClock = function () {
     var date = this.d;
     date.updateSeconds();
     var clock = document.getElementById(this.id);
@@ -42,7 +45,7 @@ Clock.prototype.updateClock = function () {
 }
 
 
-Clock.prototype.formateDigits = function (val) {
+com.ibm.Clock.prototype.formateDigits = function (val) {
     return val > 10 ? val : "0" + val;
 }
 
