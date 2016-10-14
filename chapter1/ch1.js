8357b358b54b3d5824d036912093a118ad17da4d
fudanzz <fudanzz@gmail.com>
2016-10-14 10:58:32 +0800

    all

---
+++ chapter1/ch1.js
@@ -0,0 +1,12 @@
+function onReady() {
+    console.log('hello chapter1');
+
+    var date = new Date();
+
+    console.log(date.getHours());
+    console.log(date.getHours());
+    console.log(date.getHours());
+
+}
+
+window.onload = onReady;
